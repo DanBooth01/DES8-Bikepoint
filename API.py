27ba430f8d2b4a8bd469806ae39a00fdb842e9c9
Dan-Booth-Data <DanielBooth347@Gmail.com>
2026-09-23 17:00:47 +0100

    Added a logger

--- API.py
+++ API.py
@@ -4,6 +4,7 @@ import os
 import json
 from datetime import datetime
 import time
+import logging
 
 ## API Endpoint we want to extract data from
 url = 'https://api.tfl.gov.uk/BikePoint/'
@@ -15,6 +16,24 @@ os.makedirs(data_dir, exist_ok=True)
 ## Create a timestamp
 timestamp = datetime.now().strftime("%Y-%m-%d %H-%M-&S")
 filename = f"{data_dir}/{timestamp}.json"
+
+## Create folder
+log_dir = 'log'
+os.makedirs(log_dir, exist_ok=True)
+log_filename = f"{data_dir}/{timestamp}.json"
+
+## Configure logging so messages are written to the log files
+
+logging.basicConfig(
+    filename = log_filename,
+    format = "%(asctime)s - %(levelname)s - %(message)s",
+    level = logging.INFO
+)
+
+## Create the logger and confirm that it has been successfully set up
+
+logger = logging.getLogger()
+logger.info("Logger successfuly initialised")
 
 ## Set up a rety setting in case API fails
 
@@ -38,20 +57,41 @@ while attempt < max_retry:
         ## Convert the JSON response
         data = response.json()
 
-        ##Open the output file and write the API data to it as a JSON
-        with open(filename, 'w') as file:
-            json.dump(data, file)
+        ## Check API returns data before trying to save it
 
-        print(f"File {filename} was successfully saved")
-        break
+        if len(data)>0:
+
+            try:
+                ##Open the output file and write the API data to it as a JSON
+                with open(filename, 'w') as file:
+                    json.dump(data, file)
+
+                print(f"File {filename} was successfully saved")
+
+                ##Add logger info
+                logger.info(f"File {filename} was successfully saved")
+
+            except Exception as e:
+                print(f"An error has occured: {e}")
+                logger.error(f"An error has occured: {e}")
+
+            break
+
+        # API request succeeded but no data returned
+        else: 
+            print("No data returned")
+            logger.warning("No data returned")
+            break
 
     elif status < 200 or status >=500:
                 #Wait before retying to avoid repeatadly hitting the API
         time.sleep(delay)
         attempt += 1
         print(f"Status code: {status}, retrying attempt number {attempt}")
+        logger.info(f"Status code: {status}, retrying attempt number {attempt}")
 
 
     else:
         print(f"Error. Status code {status}. Fix it")
+        logger.critical(f"Error. Status code {status}. Fix it")
         break
